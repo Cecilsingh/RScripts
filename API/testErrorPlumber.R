0184d99317a1221eb18f47b9af98d7076a300c8e
Cecil Singh <54638818+Cecilsingh@users.noreply.github.com>
2024-10-14 12:58:14 +1100

    Create testErrorPlumber.R

---
+++ API/testErrorPlumber.R
@@ -0,0 +1,11 @@
+library(plumber)
+#* @apiTitle  TestModelWithErrorMessage
+
+#* Predict on input values
+#* @post /predict
+#* @param request:list
+model.predict <- function(request) {
+  
+  stop('An error occured!')
+  
+}
